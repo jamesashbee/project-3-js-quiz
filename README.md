e063de4056dce84b52bc4dbb45401affc2d564dc
James Ashbee <jamesashbee@hotmail.com>
2022-05-10 16:01:30 +0100

    quiz game v1 10thMay22

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # project-3-js-quiz
 quiz game, created with js, html and css
+
+@ This Project has been done using
+
+  -html
+
+  -SCSS to make CSS (files styles.scss and _variables.scss)
+
+  -vanilla javascript with the following functions
+    document.querySelector
+    Arrays of Arrays
+    document.getElementById to add / remove / update CSS
+    Arrow Functions =>
+    if / else tests
+    Click Event Listeners
+    for each loops
+
+  -This Project has media queries so it will style to work on mobile, tablet and desktop monitors
+
+
+  @ The PSEUDOCODE logic includes:
+
+  On Load
+    Populate Blank Scoreboard
+    Show Start Button
+    Hide Reset Button
+
+  On Start Button Hit
+    Increment Question Number Counter
+    Popualte Question Text from Array
+    Popualte 4 Possible Answers from Array  
+    Update html of image location
+    Store in Variable Right Answer
+    Hide the Start Button and Show the Reset Button
+
+  On Answering First (and Subsequent Questions)
+      Check if button click ID is the same as the variable stored right Answer
+          
+          If the Right Answer
+              Update the Scoreboard
+              Highlight the correctly pressed answer button in Green
+              Move to the Next Question
+
+          Else The wrong Answer
+              Highlight the Wrongly pressed answer button in red
+              Update the Scoreboard to Game Over
+          
+  On Answering the Last Question Correctly
+      Update the Scoreboard to You have Completed the Quiz
+      Show the Trophy image
+
+  On Reset Button Pressed
+    Reset Question Count number to 0
+    Reset Answer Button text
+    Display on Scoreboard that Reset has been pressed
+    Show Quiz Start Image
+    Hide the Reset Button and Show the Start Button
---
+++ script.js
@@ -0,0 +1,173 @@
+// JS Quiz Game
+
+let questionCount = 0;    // To Count the number of questions 
+let correctAnswer = -1;   // to check the correct answer in the questions array
+let answerPress = "";     // to get the ID of the answer pressed
+let startQuiz = false;    // only when true will it loop to next question
+
+// score board 
+const scoreboard = document.querySelector("#user__score");
+scoreboard.innerHTML = "Are you good enought to get on the Scoreboard?";
+
+// question header 
+const questionText = document.querySelector(".quiz__question-header");
+questionText.innerHTML = "Press Start to Begin Quiz";
+
+// Dispaly Answer Buttons
+const answerButtonText = document.querySelectorAll(".quiz__answer");
+
+// quiz image
+const quizImageSource = document.querySelector("#quiz__img");
+
+// on load show start button and hide reset button
+const resetBtn = document.querySelector("#resetBtn");
+document.getElementById("resetBtn").style.visibility = "hidden";
+
+
+// Quiz questions as array
+const quizAnswersArr = [
+ [["Question 1 - What is the Capital of Wales?"],["Newport", "Cardiff", "London", "Wrexham"],[false,true,false,false],["img/wales.jpg"]],
+ [["Question 2 - What Year was the Oringal Sonic the Hedgehog Game Released?"],["1988", "1989", "1990", "1991"],[false,false,false,true],["img/sonic-game.jpg"]],
+ [["Question 3 - Who won the first Football World Cup?"],["England", "Germany", "Brazil", "Uruguay"],[false,false,false,true],["img/football-world-cup.jpg"]],
+ [["Question 4 - Which Reggae Artist has sold the most records in the UK?"],["UB40", "Bob Marley", "Shaggy", "Eddy Grant"],[true,false,false,false],["img/reggae.jpg"]],
+];
+
+
+
+// Function - Loop to next quiz question - Triggered on selecting answer
+const nextQuestion = () => {
+  
+  // if sucessfully completed last question
+  if (questionCount >= quizAnswersArr.length) {
+    questionText.innerHTML = "Well Done You Have Completed the Quiz";
+    quizImageSource.src =  "/img/winner.jpg";
+    scoreboard.innerHTML = "Quiz Champion You Got all " +(questionCount +1) +" Questions Right!";
+    startQuiz = false;   
+  }
+
+  // if still got questions to complete 
+  else{        
+    // set prev question right answer back to light blue
+    if(questionCount>0){
+    document.getElementById(answerPress).style.backgroundColor = "lightblue";
+    }
+
+    // Display Question
+    questionText.innerHTML = quizAnswersArr[questionCount][0];
+
+    // Change Text on Answer Button 
+    for (let answerButtonCount=0; answerButtonCount < 4; answerButtonCount++){
+      answerButtonText[answerButtonCount].innerHTML = quizAnswersArr[questionCount][1][answerButtonCount];
+    };
+
+    // Store which is correct answer
+   for ( answerButtonCount=0; answerButtonCount < 4; answerButtonCount++){
+      if (quizAnswersArr[questionCount][2][answerButtonCount] === true){
+      correctAnswer = answerButtonCount;
+       };
+
+    // display the image
+    quizImageSource.src =  quizAnswersArr[questionCount][3];  
+  };
+      
+    // change on screen display
+    scoreboard.innerHTML = "You are on Question " +(questionCount +1);
+  }
+
+  };
+
+  
+// reset quiz function 
+const resetQuiz = () => {
+
+  questionCount = 0;
+  questionText.innerHTML = "Press Start to Begin Quiz";
+
+  // high score - top bit of text, may get current high score 
+  scoreboard.innerHTML = "You Pressed Reset so you don't get on the Scoreboard";
+
+  // button lables = just a / b / c /d
+  const resetButtonDisplays = ["A","B","C","D"];
+  for (let answerButtonCount=0; answerButtonCount < 4; answerButtonCount++){
+    answerButtonText[answerButtonCount].innerHTML =resetButtonDisplays[answerButtonCount];
+    
+    // to add here on reset change button colour back to blue 
+    document.getElementById(answerPress).style.backgroundColor = "lightblue";
+
+    // after reset pressed hid reset and show start button
+    document.getElementById("resetBtn").style.visibility = "hidden";
+  document.getElementById("startBtn").style.visibility = "visible";
+  };
+
+  // image to blank
+  quizImageSource.src = "/img/quiz.jpg";
+
+  startQuiz = false;
+
+};
+
+
+// Start Quiz Button
+const startQuizButton = document.querySelector("#startBtn");
+startQuizButton.addEventListener("click",() => {
+
+  //alert("Start Quiz");
+  questionCount = 0;
+  scoreboard.innerHTML = "Good Luck With the Quiz";
+ 
+  nextQuestion();
+
+  startQuiz = true;
+
+  // after start pressed hide start and show reset button
+  document.getElementById("resetBtn").style.visibility = "visible";
+  document.getElementById("startBtn").style.visibility = "hidden";
+});
+
+
+
+// Reset Button listener
+resetBtn.addEventListener("click",() => {
+ resetQuiz();
+});
+
+
+// Quiz Answer Pressed
+const answerButtonsPress = document.querySelectorAll(".quiz__answer");
+answerButtonsPress.forEach((answerBtnPress) => {
+  answerBtnPress.addEventListener("click", () => {
+
+  // need error if have not started or at end
+    if (startQuiz === true) {
+    answerPress = answerBtnPress.id;
+
+    // check to see if answer is correct
+    if(answerPress == correctAnswer){
+
+      // css answer in green
+      document.getElementById(answerPress).style.backgroundColor = "#03EF62";  // green
+
+      questionCount = questionCount +1;
+
+     nextQuestion();
+    }
+
+    else{
+      // update scoreboard to final score / game over 
+      scoreboard.innerHTML = `Game Over, you got ${questionCount} questions Right...`;
+
+      // css answer in red
+      document.getElementById(answerPress).style.backgroundColor = "red";
+
+      // lock quiz so have to reset
+      startQuiz = false;
+    };
+ 
+  }
+
+else {
+  return;
+}
+
+  });
+});
